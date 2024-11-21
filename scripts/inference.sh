fc9905cf024adb7db1f8189b6f2fcb61be03c56a
xkaska02 <xkaska02@fit.vutbr.cz>
2024-11-21 11:56:26 +0100

    chore: add commented czert path to inference script

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -4,9 +4,10 @@ BASE_DIR="/mnt/c/Users/kaska/Documents/vysoka_skola/form-data-extraction"
 SCRIPT_DIR=$BASE_DIR/code
 SAVE_DIR=$BASE_DIR/models_tmp
 DATAFILES_DIR=$BASE_DIR/data_files
-MODEL_CHECKPOINT=checkpoint-900
+MODEL_CHECKPOINT=checkpoint-450
 
     
+    # --model_path "UWB-AIR/Czert-B-base-cased" \
 python3 $SCRIPT_DIR/inference.py \
     --model_path $SAVE_DIR/$MODEL_CHECKPOINT \
     --train_file $DATAFILES_DIR/train.parquet \
