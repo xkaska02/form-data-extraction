a3d710e0d941b19e4aaa25a095d1851ad1bea823
xkaska02 <xkaska02@fit.vutbr.cz>
2025-05-09 04:13:13 +0200

    add lilt experiments

---
+++ scripts/lilt_loss_on_subtokens.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+BASE_DIR="/home/kaska/BP/form-data-extraction"
+
+source $BASE_DIR/venv/bin/activate
+
+SCRIPT_DIR=$BASE_DIR/code
+SAVE_DIR=$BASE_DIR/models_tmp/forms/model
+DATAFILES_DIR=$BASE_DIR/data_files/forms_json_dataset
+MODEL=nielsr/lilt-xlm-roberta-base
+
+mkdir -p $SAVE_DIR
+
+python3 $SCRIPT_DIR/train_lilt.py \
+    --save_path $SAVE_DIR \
+    --model_path $MODEL \
+    --model_name lilt-xlm-roberta \
+    --train_file $DATAFILES_DIR/train_split.json \
+    --test_file $DATAFILES_DIR/test_split.json \
+    --validation_file $DATAFILES_DIR/validation_split.json \
+    --lr 2e-5 \
+    --batch_size 4 \
+    --epochs 30 \
+    --decay 0.01 \
+    --eval_strat epoch \
+    --save_strat epoch \
+    --classifier_head_layers 1 \
+    --loss_on_subtokens False \
+    --experiment_name loss_on_subtokens_lilt
+
+python3 $SCRIPT_DIR/train_lilt.py \
+    --save_path $SAVE_DIR \
+    --model_path $MODEL \
+    --model_name lilt-xlm-roberta \
+    --train_file $DATAFILES_DIR/train_split.json \
+    --test_file $DATAFILES_DIR/test_split.json \
+    --validation_file $DATAFILES_DIR/validation_split.json \
+    --lr 2e-5 \
+    --batch_size 4 \
+    --epochs 30 \
+    --decay 0.01 \
+    --eval_strat epoch \
+    --save_strat epoch \
+    --classifier_head_layers 1 \
+    --loss_on_subtokens True \
+    --experiment_name loss_on_subtokens_lilt
